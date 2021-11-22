102eba62a59a1f8d0de2237f73d290d7bdd03933
rdwanmuhamad <mrdwn624@gmail.com>
2021-11-22 20:52:08 +0800

    design web

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import img01 from './assets/img/img-01.jpg'
 
 function App() {
+  const category = "Gundam Series"
+  const title = "Gundam Barbatos"
+  const price = 300000
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,20 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+    <div className="App-Photo">
+      <img src={img01} />
+    </div>
+    <div className="App-details">
+      <p className="App-category">{category}</p>
+      <h1 className="App-title">{title}</h1>
+      <h1 className="App-price">IDR {price}</h1>
+      <p className="App-deskripsi">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Sit, saepe. Voluptatem ullam illum tempora maxime? 
+          Magnam quam est fugiat aliquid sit doloremque voluptates commodi, soluta pariatur qui quae. 
+          Omnis, quasi!
+      </p>
+    </div>
     </div>
   );
 }
