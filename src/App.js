aa9f9122b20751fec2c499ccc79bebe8fb69d3ed
rdwanmuhamad <mrdwn624@gmail.com>
2021-11-23 13:25:25 +0800

    split & import component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <ProductGalleries/>
       <ProductDetails isDiscount = "yes" price = "300.000" category = "Gundam Series" title = "Gundam Barbatos" />
-      <ReviewItem/>
+      {/* <ReviewItem/> */}
     </div>
   );
 }
@@ -66,41 +66,5 @@ function AddCart(e){
   return console.log("Membeli Produk " + e);
 }
 
-function ReviewItem(){
-  // mempersiapkan data dummy JSON
-  const users = [
-    {
-      "id": 1,
-      "name": "Muhamad Ridwan",
-      "review": "Pengiriman cepat, harga terjangkau dan kualitas barang ori. Keren!"
-    },
-    {
-      "id": 2,
-      "name": "Muhamad Ridwan",
-      "review": "Pengiriman cepat, harga terjangkau dan kualitas barang ori. Keren!"
-    },
-    {
-      "id": 3,
-      "name": "Muhamad Ridwan",
-      "review": "Pengiriman cepat, harga terjangkau dan kualitas barang ori. Keren!"
-    },
-  ];
-  const listReview = users.map((itemReview) =>
-    <div>
-      <div className="Image-review">
-        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-      </div>
-      <div className="User-review">
-        <h3>{itemReview.name}</h3>
-        <p>{itemReview.review}</p>
-      </div>
-    </div>
-  );
-  return (
-    <div className="App-review">
-        <h2>Review</h2>
-        {listReview}
-    </div>
-  );
-}
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Review from './Review';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Review/>
   </React.StrictMode>,
   document.getElementById('root')
 );
