c5de53217a5868afb246ff05f9901478f0dc9d12
rdwanmuhamad <mrdwn624@gmail.com>
2021-11-23 10:56:29 +0800

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ function App() {
   return (
     <div className="App">
       <ProductGalleries/>
-      <ProductDetails price = "300.000" category = "Gundam Series" title = "Gundam Barbatos" />
+      <ProductDetails isDiscount = "yes" price = "300.000" category = "Gundam Series" title = "Gundam Barbatos" />
     </div>
   );
+}
+
+function CheckDiscount(props){
+  const {isDiscount} = props;
+  if(isDiscount == "yes") {
+    return (
+      <p>Diskon 50%</p>
+    );
+  } else if (isDiscount == "coming") {
+    return (
+      <p>Coming soon...</p>
+    );
+  } else{
+    return (
+      <p>Belum ada diskon</p>
+    )
+  }
 }
 
 function ProductGalleries(){
@@ -20,12 +37,13 @@ function ProductGalleries(){
 }
 
 function ProductDetails(props){
-  const {category, title, price} = props;
+  const {category, title, price, isDiscount} = props;
   return (
     <div className="App-details">
       <p className="App-category">{category}</p>
       <h1 className="App-title">{title}</h1>
       <h1 className="App-price">IDR {price},-</h1>
+      <CheckDiscount isDiscount={isDiscount} />
       <p className="App-deskripsi">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Sit, saepe. Voluptatem ullam illum tempora maxime? 
           Magnam quam est fugiat aliquid sit doloremque voluptates commodi, soluta pariatur qui quae. 
